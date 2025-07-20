e3dc03221363429000ffb628ff1f19d6225f1f09
zimejin07 <zimejin@gmail.com>
2025-07-20 14:52:02 +0100

    chore: fix docker test and update ts config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,7 @@
 // Importing the Config type from jest to ensure the config object adheres to the expected shape.
 import type { Config } from "jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
 
 // Define a configuration object for the testing framework
 const config: Config = {
@@ -20,6 +22,14 @@ const config: Config = {
     // both TypeScript and JavaScript files for testing.
     "^.+.(ts|tsx|js|jsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
+
+  // The moduleNameMapper is a configuration option used in Jest to resolve module paths.
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    // pathsToModuleNameMapper is a utility function from the 'ts-jest' library that allows
+    // the conversion of TypeScript's "paths" mappings in the tsconfig.json file into Jest's
+    // module name mapping format.
+    prefix: "<rootDir>/",
+  }),
 
   // Scripts or modules to run before each test file is executed
   setupFiles: ["<rootDir>/tests/setup-env.ts"], // Path to initial setup file for test environment
--- tests/api/auth.test.ts
+++ tests/api/auth.test.ts
@@ -5,7 +5,8 @@ describe("Middleware Auth Redirect", () => {
   it("redirects unauthenticated user from /admin", async () => {
     const res = await request(baseUrl).get("/admin");
 
-    expect(res.status).toBe(302);
+    expect(res.status).toBeGreaterThanOrEqual(300);
+    expect(res.status).toBeLessThan(400);
     expect(res.headers.location).toBe("/login");
   });
 });
